9024cdf430a2b17ca5a7df1300284be3d027c583
caeserlettuce <caeserlettuce@gmail.com>
2023-11-21 19:09:11 -0800

    big update
    big

--- process.py
+++ process.py
@@ -10,6 +10,7 @@ import logging
 import time
 import zipfile
 import shutil
+import ffmpeg
 now = datetime.now()
 
 
@@ -33,6 +34,22 @@ filetype_groups = {   # capitals dont matter, it compares them all in lowercase
 }
 
 input_folder = "in"
+output_folder = "out"
+
+month_folder_names = {
+  1: "01 january",
+  2: "02 february",
+  3: "03 march",
+  4: "04 april",
+  5: "05 may",
+  6: "06 june",
+  7: "07 july",
+  8: "08 august",
+  9: "09 september",
+  10: "10 october",
+  11: "11 november",
+  12: "12 december"
+}
 
 def exit_handler():
   print(bc.GREEN + "t'as been closed" + bc.END)
@@ -67,9 +84,29 @@ def get_file_info(path, name_info):
       info_out["metadate"] = exif[36867]
     else:
       info_out["metadate"] = False
-  else:
-    info_out["metadate"] = False
+  elif (name_info["type"].lower() in filetype_groups["video"]):
+    video_metadata = ffmpeg.probe(path)["streams"]
+    if type(video_metadata) == type([]):
+     for i in video_metadata:
+       if "tags" in i:
+         # has tags!!
+         if "creation_time" in i["tags"]:
+           # has video created!!!! tag!!! metadata!!!! omg!!!
+           info_out["vidodate"] = str(i["tags"]["creation_time"]).split(".")[0]
+    else:
+     if "tags" in video_metadata:
+       # has tags!!
+       if "creation_time" in video_metadata["tags"]:
+         # has video created!!!! tag!!! metadata!!!! omg!!!
+         info_out["vidodate"] = str(video_metadata["tags"]["creation_time"]).split(".")[0]
+
   info_out["filedate"] = time.ctime(os.path.getmtime(path))
+  if (info_out.get("vidodate", False) == False):
+    info_out["vidodate"] = False
+  if (info_out.get("metadate", False) == False):
+    info_out["metadate"] = False
+  if (info_out.get("filedate", False) == False):
+    info_out["filedate"] = False
   return info_out
 
 print(bc.HEADER + "processing the photos now!!!" + bc.END) # yippee!!
@@ -79,8 +116,76 @@ big_file_list = os.listdir(input_folder)
 
 
 
-def process_file(path):
-  print("processing file " + path + "...")
+def process_file(filename):
+  print("processing file " + filename + "...")
+
+  if settings["sort by month/year"] == True:
+    # sort
+    info = get_file_info(os.path.join(input_folder, filename), get_filename_info(filename))  # the info
+    metadate = False
+    filedate = False
+    vidodate = False
+
+    if info["metadate"] != False:
+      metadate = datetime.strptime(info["metadate"], "%Y:%m:%d %H:%M:%S")
+    if info["filedate"] != False:
+      filedate = datetime.strptime(info["filedate"], "%a %b %d %H:%M:%S %Y")
+    if info["vidodate"] != False:
+      vidodate = datetime.strptime(info["vidodate"], "%Y-%m-%dT%H:%M:%S")
+    print(bc.BLUE + "metadate: " + bc.GREEN + str(metadate) + bc.END)
+    print(bc.BLUE + "filedate: " + bc.GREEN + str(filedate) + bc.END)
+    print(bc.BLUE + "vidodate: " + bc.GREEN + str(vidodate) + bc.END)
+    month = 0
+    year = 0
+    if metadate == False and vidodate == False:
+      if settings["use modified date as backup"] == True:
+        month = filedate.month
+        year = filedate.year
+      else:
+        month = False
+        year = False
+    elif metadate == False and vidodate != False:
+      month = vidodate.month
+      year = vidodate.year
+    else:
+      month = metadate.month
+      year = metadate.year
+    print(bc.BLUE + "month: " + bc.GREEN + bc.UNDERLINE + str(month) + bc.END)
+    print(bc.BLUE + "year: " + bc.GREEN + bc.UNDERLINE + str(year) + bc.END)
+
+    if type(year) != type(1):
+      # no year!!
+      year = "unknown"
+    if month in month_folder_names:
+      month = month_folder_names[month]
+    else:
+      # no month!!
+      month = False
+
+    folder_check(os.path.join(output_folder, str(year)))              # check year folder
+    if (month != False):
+      folder_check(os.path.join(output_folder, str(year), str(month)))  # check month folder
+      if settings["output to iphone folders"] == True:
+        folder_check(os.path.join(output_folder, str(year), str(month), "iphone"))  # check iphone folder
+    else:
+      if settings["output to iphone folders"] == True:
+        folder_check(os.path.join(output_folder, str(year), "iphone"))  # check iphone folder
+    
+    print(bc.GREEN + "saving file " + bc.BLUE + bc.UNDERLINE + filename + bc.END + bc.GREEN + "!" + bc.END)
+    if (month == False):
+      if settings["output to iphone folders"] == True:
+        shutil.move(os.path.join(input_folder, filename), os.path.join(output_folder, str(year), "iphone", filename))
+      else:
+        shutil.move(os.path.join(input_folder, filename), os.path.join(output_folder, str(year), filename))
+    else:
+      if settings["output to iphone folders"] == True:
+        shutil.move(os.path.join(input_folder, filename), os.path.join(output_folder, str(year), str(month), "iphone", filename))
+      else:
+        shutil.move(os.path.join(input_folder, filename), os.path.join(output_folder, str(year), str(month), filename))
+  else:
+    # dont sort
+    print(bc.GREEN + "saving file " + bc.BLUE + bc.UNDERLINE + filename + bc.END + bc.GREEN + "!" + bc.END)
+    shutil.move(os.path.join(input_folder, filename), os.path.join(output_folder, filename))
 
 
 
@@ -101,7 +206,9 @@ for filename in big_file_list:
     os.rmdir(os.path.join(input_folder, 'iCloud Photos'))
     os.remove(f_path)
 
+big_file_list = os.listdir(input_folder)
 
+file_process_list = []
 
 for filename in big_file_list:
   f_path = os.path.join(input_folder, filename)
@@ -119,25 +226,25 @@ for filename in big_file_list:
   if (nameinfo["type"].lower() in filetype_groups["archive"]):  # archive file
     print(bc.CYAN + "it's an archive file!" + bc.END)
 
-    #zip = zipfile.ZipFile(f_path)
-    
-    #icloud_folder = zip.open()
     
 
   elif (nameinfo["type"].lower() in filetype_groups["photo"] ): # photo file
     print(bc.CYAN + "it's a photo file!" + bc.END)
-    process_file(f_path)
+    file_process_list.append(filename)
   elif (nameinfo["type"].lower() in filetype_groups["video"] ): # video file
     print(bc.CYAN + "it's a video file!" + bc.END)
     if settings["delete live photos"] == True:
       # delete if matching file
       possible_name = nameinfo["name"] + ".JPG"
+      print(possible_name)
       if os.path.exists(os.path.join(input_folder, possible_name)):
         # it exists!
-        print(bc.WARNING + "photo " + bc.UNDERLINE + possible_name + bc.END + bc.WARNING + " exists! skipping processing the video.")
+        print(bc.WARNING + "photo " + bc.UNDERLINE + possible_name + bc.END + bc.WARNING + " exists! deleting the video.")
+        os.remove(f_path)
       else:
-        process_file(f_path)
+        file_process_list.append(filename)
     else:
-      process_file(f_path)
+      file_process_list.append(filename)
 
-
+for file in file_process_list:  # if i make it process in the function above, the live video deletey thing doesnt work cause itll move the photos before the videos
+  process_file(file)
